06263fcba830ddd6009d92b4df1c67a167027859
yashkataraei <yashkataraei@gmail.com>
2025-07-16 17:32:25 +0530

    commit

--- cricket-frontend/src/app/login/login.component.ts
+++ cricket-frontend/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormsModule ,FormControl,Validators,FormGroup} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthserviceService } from '../authservice.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+loginForm!: FormGroup;
+authError: string = '';
+isError: any = "";
+
+constructor(private authService: AuthserviceService, private router: Router) {}
+
+ngOnInit() {
+    this.loginForm = new FormGroup({
+      userId: new FormControl('', [Validators.required, Validators.maxLength(20)]),
+      password: new FormControl('', [Validators.required,Validators.maxLength(16)])
+    });
+  }
+login() {
+  if(this.loginForm.valid) {
+   console.log(this.loginForm.value);
+
+    ;
+  }
+}
 
 }
